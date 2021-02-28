a81ea3c5827aadedb9de61ae6782e60005a0e165
New0123 <new2jo@gmail.com>
2021-02-28 20:39:51 +0300

    made changes to files

--- app/__init__.py
+++ app/__init__.py
@@ -9,16 +9,14 @@ from flask_login import LoginManager
 from config import Config
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/postgres'
+app = Flask(__name__)
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-def create_app(config_class=Config):
-app = Flask(__name__)
-app.config.from_object(config_class)
-
-if not app.debug and not app.testing:
+if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -33,26 +31,6 @@ if not app.debug and not app.testing:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-
-    if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/microblog.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup')
-
-    return app
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
